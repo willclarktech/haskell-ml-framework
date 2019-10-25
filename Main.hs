9d8960f99ec74bf14c5350c3a9aee0552c971f5c
willclarktech <willclarktech@users.noreply.github.com>
2019-10-25 15:04:48 +0200

    :seedling: forward: Add forwardPropagateInput function with linear layer

--- Main.hs
+++ Main.hs
@@ -1,3 +1,12 @@
 type Activation = Float
 type Weight = Float
 type Bias = Float
+
+forwardPropagateInput :: Activation -> Activation
+forwardPropagateInput input =
+	let
+		weight = 2.5 :: Weight
+		bias = (- 4.0) :: Bias
+		linearLayer = input * weight + bias
+	in
+		linearLayer
