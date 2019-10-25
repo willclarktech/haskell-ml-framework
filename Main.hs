ece91b90c7664ef3cb6c862ade89f2f139b3b6d7
willclarktech <willclarktech@users.noreply.github.com>
2019-10-25 15:19:28 +0200

    :seedling: forward: Separate out applyNonLinearLayer function

--- Main.hs
+++ Main.hs
@@ -5,12 +5,15 @@ type Bias = Float
 applyLinearLayer :: Weight -> Bias -> Activation -> Activation
 applyLinearLayer weight bias = (+ bias) . (* weight)
 
+applyNonLinearLayer :: Activation -> Activation
+applyNonLinearLayer = (1 /) . (1 +) . exp . (0 -)
+
 forwardPropagateInput :: Activation -> Activation
 forwardPropagateInput input =
 	let
 		weight = 2.5 :: Weight
 		bias = (- 4.0) :: Bias
 		linearLayer = applyLinearLayer weight bias input
-		nonLinearLayer = 1 / (1 + exp (-linearLayer))
+		nonLinearLayer = applyNonLinearLayer linearLayer
 	in
 		nonLinearLayer
