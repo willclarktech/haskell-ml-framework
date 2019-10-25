9ea93245536ed79b5ec00515e4a05e5374f80e46
willclarktech <willclarktech@users.noreply.github.com>
2019-10-25 15:13:50 +0200

    :seedling: forward: Add non-linear layer to forwardPropagateInput

--- Main.hs
+++ Main.hs
@@ -8,5 +8,6 @@ forwardPropagateInput input =
 		weight = 2.5 :: Weight
 		bias = (- 4.0) :: Bias
 		linearLayer = input * weight + bias
+		nonLinearLayer = 1 / (1 + exp (-linearLayer))
 	in
-		linearLayer
+		nonLinearLayer
