2d909e2626fc7df7a967722e5a968e5a26e43ad3
willclarktech <willclarktech@users.noreply.github.com>
2019-10-26 11:22:16 +0200

    :recycle: repo: Separate out Layer module

---
+++ Layer.hs
@@ -0,0 +1,62 @@
+module Layer where
+
+import System.Random
+import MlMath
+
+type Activation = Float
+type Weight = Float
+type Bias = Float
+type Width = Int
+
+data Layer =
+	LinearLayer
+		{ weights :: [[Weight]]
+		, biases :: [Bias]
+		}
+	| NonLinearLayer
+		{ function :: NonLinearFunction
+		}
+
+data LayerSpecification =
+	LinearLayerSpecification Width
+	| NonLinearLayerSpecification String
+
+applyLinearLayer :: Layer -> [Activation] -> [Activation]
+applyLinearLayer (LinearLayer weights biases) input =
+	let weightedSums = vectorMatrixMultiplication input weights
+	in zipWith (+) biases weightedSums
+applyLinearLayer _ _ = error "Cannot apply non-linear layer"
+
+applyNonLinearLayer :: Layer -> [Activation] -> [Activation]
+applyNonLinearLayer (NonLinearLayer function) = map function
+applyNonLinearLayer _ = error "Cannot apply linear layer"
+
+applyLayer :: [Activation] -> Layer -> [Activation]
+applyLayer input layer =
+	case layer of
+		LinearLayer _ _ -> applyLinearLayer layer input
+		NonLinearLayer _ -> applyNonLinearLayer layer input
+
+getRandomValues :: StdGen -> [Float]
+getRandomValues = randomRs (-1.0, 1.0)
+
+initializeBiases :: StdGen -> Width -> [Bias]
+initializeBiases g width = take width $ getRandomValues g
+
+initializeWeights :: StdGen -> Width -> Width -> [[Weight]]
+initializeWeights g previousWidth width =
+	let
+		randomValues = take (previousWidth * width) $ getRandomValues g
+		foldFn (ws, rs) _ =
+			let (rsNow, rsNext) = splitAt previousWidth rs
+			in (rsNow:ws, rsNext)
+		(weights, _) = foldl foldFn ([], randomValues) (replicate previousWidth 0)
+	in weights
+
+createLinearLayer :: StdGen -> Int -> Int -> Layer
+createLinearLayer g previousWidth width =
+	let (g1, g2) = split g
+	in LinearLayer (initializeWeights g1 previousWidth width) (initializeBiases g2 width)
+
+createNonLinearLayer :: String -> Layer
+createNonLinearLayer = NonLinearLayer . resolveNonLinearFunction
--- Lib.hs
+++ Lib.hs
@@ -1,46 +1,9 @@
 module Lib where
 
 import System.Random
-import MlMath
-
-type Activation = Float
-type Weight = Float
-type Bias = Float
-type Width = Int
-
-data Layer =
-	LinearLayer
-		{ weights :: [[Weight]]
-		, biases :: [Bias]
-		}
-	| NonLinearLayer
-		{ function :: NonLinearFunction
-		}
-
-data LayerSpecification =
-	LinearLayerSpecification Width
-	| NonLinearLayerSpecification String
+import Layer
 
 type Network = [Layer]
-
-applyLinearLayer :: Layer -> [Activation] -> [Activation]
-applyLinearLayer (LinearLayer weights biases) input =
-	let weightedSums = vectorMatrixMultiplication input weights
-	in zipWith (+) biases weightedSums
-applyLinearLayer _ _ = error "Cannot apply non-linear layer"
-
-applyNonLinearLayer :: Layer -> [Activation] -> [Activation]
-applyNonLinearLayer (NonLinearLayer function) = map function
-applyNonLinearLayer _ = error "Cannot apply linear layer"
-
-applyLayer :: [Activation] -> Layer -> [Activation]
-applyLayer input layer =
-	case layer of
-		LinearLayer _ _ -> applyLinearLayer layer input
-		NonLinearLayer _ -> applyNonLinearLayer layer input
-
-forwardPropagateInput :: Network -> [Activation] -> [Activation]
-forwardPropagateInput network input = foldl applyLayer input network
 
 getOutputWidth :: Width -> Network -> Width
 getOutputWidth inputWidth [] = inputWidth
@@ -48,30 +11,6 @@ getOutputWidth inputWidth network =
 	case last network of
 		LinearLayer weights _ -> length weights
 		NonLinearLayer _ -> getOutputWidth inputWidth $ init network
-
-getRandomValues :: StdGen -> [Float]
-getRandomValues = randomRs (-1.0, 1.0)
-
-initializeBiases :: StdGen -> Width -> [Bias]
-initializeBiases g width = take width $ getRandomValues g
-
-initializeWeights :: StdGen -> Width -> Width -> [[Weight]]
-initializeWeights g previousWidth width =
-	let
-		randomValues = take (previousWidth * width) $ getRandomValues g
-		foldFn (ws, rs) _ =
-			let (rsNow, rsNext) = splitAt previousWidth rs
-			in (rsNow:ws, rsNext)
-		(weights, _) = foldl foldFn ([], randomValues) (replicate previousWidth 0)
-	in weights
-
-createLinearLayer :: StdGen -> Int -> Int -> Layer
-createLinearLayer g previousWidth width =
-	let (g1, g2) = split g
-	in LinearLayer (initializeWeights g1 previousWidth width) (initializeBiases g2 width)
-
-createNonLinearLayer :: String -> Layer
-createNonLinearLayer = NonLinearLayer . resolveNonLinearFunction
 
 appendLayer :: Width -> (StdGen, Network) -> LayerSpecification -> (StdGen, Network)
 appendLayer inputWidth (g, network) specification =
@@ -87,3 +26,6 @@ createNetwork :: StdGen -> Width -> [LayerSpecification] -> Network
 createNetwork g inputWidth layerSpecifications =
 	let (_, network) = foldl (appendLayer inputWidth) (g, []) layerSpecifications
 	in network
+
+forwardPropagateInput :: Network -> [Activation] -> [Activation]
+forwardPropagateInput network input = foldl applyLayer input network
